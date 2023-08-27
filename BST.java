0ac557fcbb2dd5a437f0065519edb39c773a4d9f
Faisal Hossain <faisal.hossain75@gmail.com>
2023-08-27 15:55:12 -0500

    Add BST add and remove functions

--- BST.java
+++ BST.java
@@ -32,10 +32,16 @@ public class BST<T extends Comparable<? super T>> {
      * @throws java.lang.IllegalArgumentException If data is null.
      */
     public void add(T data) {
-       root = rAdd(root, data);
+
+        // Error Handling: Invalid data
+        if (data == null) {
+            throw new java.lang.IllegalArgumentException("Error: Provided data is null.");
+        }
+
+        root = rAdd(root, data);
     }
 
-    public BSTNode<T> rAdd(BSTNode<T> curr, T data) {
+    private BSTNode<T> rAdd(BSTNode<T> curr, T data) {
 
         // Base case: Found position where to add data
         if (curr == null) {
@@ -43,14 +49,14 @@ public class BST<T extends Comparable<? super T>> {
             return new BSTNode<T>(data);
         }
 
-        // Point to right, if current value is smaller than data value
+        // Current value is larger than data value, traverse to left node
         else if ((curr.getData().compareTo(data)) > 0) {
-            return curr.getRight();
+            curr.setLeft(rAdd(curr.getLeft(), data));
         }
 
-        // Point to left, if current value is smaller than data value
+        // Current value is smaller than data value, traverse to right node
         else if ((curr.getData().compareTo(data)) < 0) {
-            return curr.getLeft();
+            curr.setRight(rAdd(curr.getRight(), data));
         }
 
         return curr;
@@ -83,30 +89,46 @@ public class BST<T extends Comparable<? super T>> {
      * @throws java.lang.IllegalArgumentException If data is null.
      * @throws java.util.NoSuchElementException   If the data is not in the tree.
      */
-    public void remove(T data) {
-       root = rRemove(root, data);
+    public T remove(T data) {
+
+        // Error Handling: Invalid data
+        if (data == null) {
+            throw new java.lang.IllegalArgumentException("Error: Provided data is null.");
+        }
+
+        // Error Handling: Invalid data
+        if (size == 0) {
+            throw new java.lang.IllegalArgumentException("Error: Empty tree.");
+        }
+
+        BSTNode<T> returnNode = new BSTNode<T>(null);
+
+        root = rRemove(root, data, returnNode);
+
+        return returnNode.getData();
     }
 
-    public BSTNode<T> rRemove(BSTNode<T> curr, T data) {
+    private BSTNode<T> rRemove(BSTNode<T> curr, T data, BSTNode<T> returnData) {
 
         // Error Handling: Data doesn't exist in tree
         if (curr == null) {
-            throw new java.lang.IllegalArgumentException("Error: Data does not exist in tree.");
+            throw new NoSuchElementException("Error: Data does not exist in tree.");
         }
 
-        // Point to right, if current value is smaller than data value
+        // Current value is larger than data value, traverse to left node
         else if ((curr.getData().compareTo(data)) > 0) {
-            return curr.getRight();
+            curr.setLeft(rRemove(curr.getLeft(), data, returnData));
         }
 
-        // Point to left, if current value is smaller than data value
+        // Current value is smaller than data value, Add left
         else if ((curr.getData().compareTo(data)) < 0) {
-            return curr.getLeft();
+            curr.setRight(rRemove(curr.getRight(), data, returnData));
         }
 
         // Base case: Found position of data value
         else if ((curr.getData().compareTo(data)) == 0) {
             size--;
+            returnData.setData(curr.getData());
 
             // Case 1: Leaf Node
             if (curr.getLeft() == null && curr.getRight() == null) {
@@ -122,10 +144,35 @@ public class BST<T extends Comparable<? super T>> {
                 return curr.getRight();
             }
 
-            // Case 3: Two Child Node
+            // Case 3: Two Child Node - successor
+            else {
+                BSTNode<T> dummyNode = new BSTNode<>(null);
+                // Set right nodes
+                curr.setRight(removeSuccessor(curr.getRight(), dummyNode));
+                // Replace the current node's value with the successor value
+                curr.setData(dummyNode.getData());
+            }
         }
 
         return curr;
+    }
+    
+    private BSTNode<T> removeSuccessor(BSTNode<T> curr, BSTNode<T> dummy) {
+
+            // Base Case - Find last left node
+            if (curr.getLeft() == null) {
+                dummy.setData(curr.getData());
+                // Removes the current node from the tree,
+                // making the right child None or another subtree
+                return curr.getRight();
+            }
+
+            else {
+                // Add nodes
+                curr.setLeft(removeSuccessor(curr.getLeft(), dummy));
+            }
+
+            return curr;
     }
 
     /**
